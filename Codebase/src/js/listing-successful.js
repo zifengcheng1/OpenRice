258cd4d65b66570fe8c4c92be492a2917f35b037
zifengcheng1 <zifengcheng1@gmail.com>
2024-09-18 15:41:02 +1000

    Fix dynamic display of locker number not being accurate

--- Codebase/src/js/listing-successful.js
+++ Codebase/src/js/listing-successful.js
@@ -1,7 +1,11 @@
 function lockerNum() {
-    const lockers = [1,2,3,4];
+    const lockers = [1,2,3,4,5];
     var emptyLocker = lockers.findIndex(isEmpty);
     if (emptyLocker >= 0) {
+        if (emptyLocker == 4){
+            localStorage.setItem("Full", "true")
+            return 4;
+        }
         return lockers[emptyLocker];
     } 
     return 0;
@@ -14,11 +18,14 @@ function isEmpty(num) {
     return false;
 }
 
-var emptyLockerNum = lockerNum();
-if (emptyLockerNum > 0) {
-    emptyLockerNum = emptyLockerNum - 1;
-    document.getElementById('locker').innerText = emptyLockerNum.toString();
-} else {
+if (localStorage.getItem("lockersFilled") == null) {
+    localStorage.setItem("lockersFilled", "1");
+    document.getElementById('locker').innerText = "1";
+} else if (localStorage.getItem("lockersFilled") == "4") {
     document.getElementById("title").innerText = "Listing failed!";
     document.getElementById("message").innerText = "Sorry, all lockers are full. Please try again later";
+} else {
+    var lockerNum = Number(localStorage.getItem("lockersFilled")) + 1;
+    document.getElementById('locker').innerText = lockerNum.toString();
+    localStorage.setItem("lockersFilled", lockerNum.toString());
 }
